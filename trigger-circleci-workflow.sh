0775c5f8ff85307c94f3a4eb2522c7884941fa79
Paul Ness <paulness@artnet.com>
2018-12-13 12:26:32 -0500

    Update trigger to use inbuilt env vars https://developer.github.com/actions/creating-github-actions/accessing-the-runtime-environment/#environment-variables

--- trigger-circleci-workflow.sh
+++ trigger-circleci-workflow.sh
@@ -2,6 +2,12 @@
 
 set -eu
 
+GITHUB_OWNER="$(echo "$GITHUB_REPOSITORY" | cut -f1 -d/)"
+GITHUB_REPO="$(echo "$GITHUB_REPOSITORY" | cut -f2 -d/)"
+GITHUB_BRANCH="$(echo "$GITHUB_REF" | cut -c 12-)"
+
+echo "Triggering CircleCI workflow for the following. $GITHUB_OWNER/$GITHUB_REPO - $GITHUB_BRANCH"
+
 curl -u "$CIRCLE_TOKEN": \
 	-X POST --header "Content-Type: application/json" \
 	-d "{""branch"":""$GITHUB_BRANCH""}" \
