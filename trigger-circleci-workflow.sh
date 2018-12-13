e54168b1666edfcec55f59e92ac117ee5abc09c8
Paul Ness <paulness@artnet.com>
2018-12-13 15:14:56 -0500

    Add fail on error.

--- trigger-circleci-workflow.sh
+++ trigger-circleci-workflow.sh
@@ -20,6 +20,7 @@ GITHUB_BRANCH="$(echo "$GITHUB_REF" | cut -c 12-)"
 echo "Triggering CircleCI workflow for the following. $GITHUB_OWNER/$GITHUB_REPO - $GITHUB_BRANCH"
 
 curl -u "$CIRCLE_TOKEN": \
+    --silent --show-error --fail \
 	-X POST --header "Content-Type: application/json" \
 	-d "{\"branch\":\"$GITHUB_BRANCH\"}" \
 	"https://circleci.com/api/v1.1/project/github/$GITHUB_OWNER/$GITHUB_REPO/build"
